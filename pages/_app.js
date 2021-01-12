a38636b4a0874d6efcc8477e28bade7dfdd78b43
gie <gie68i@gmail.com>
2021-01-12 09:50:50 +0700

    added theme and prepared for chakra-ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,40 @@
+import { ThemeProvider, CSSReset } from '@chakra-ui/react';
+import { Global, css } from '@emotion/react';
+
 import { AuthProvider } from '../lib/auth';
 import '../styles/globals.css';
 
+import customTheme from '@/styles/theme';
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
+
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ThemeProvider theme={customTheme}>
+      <GlobalStyle />
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
 
