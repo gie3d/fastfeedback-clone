2bf42cc5dc427d33ddc86f6f036e03bfbed619a2
gie <gie68i@gmail.com>
2021-01-16 13:48:30 +0700

    add site and refresh using swr with mutation

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,7 +1,31 @@
+import useSWR from 'swr';
+
 import EmptyState from '../components/EmptyState';
+import SiteTableSkeleton from '@/components/SiteTableSkeleton';
+import SiteTable from '@/components/SiteTable';
+import DashboardShell from '@/components/DashboardShell';
+import fetcher from '@/utils/fetcher';
+import { useAuth } from '@/lib/auth';
 
 const Dashboard = () => {
-  return <EmptyState />;
+  const auth = useAuth();
+  const { data, error } = useSWR('/api/sites', fetcher);
+
+  console.log('data is: ', data, error);
+
+  if (!data) {
+    return (
+      <DashboardShell>
+        <SiteTableSkeleton />
+      </DashboardShell>
+    );
+  }
+
+  return (
+    <DashboardShell>
+      {data.sites ? <SiteTable sites={data.sites} /> : <EmptyState />}
+    </DashboardShell>
+  );
 };
 
 export default Dashboard;
